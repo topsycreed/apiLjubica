3f8ebe763597d2f8372cdbe20c2a927518904d33
gchursov <topsycreed@gmail.com>
2025-03-16 18:29:27 +0100

    Add pet tests

--- src/test/java/dto/Category.java
+++ src/test/java/dto/Category.java
@@ -1,4 +1,22 @@
 package dto;
 
 public class Category {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+    }
 }
